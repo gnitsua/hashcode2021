dfd4e67982aaa8be548234b8fc3bacc052068538
mudkipmaster <acg59@drexel.edu>
2021-02-25 12:24:06 -0800

    Progress commit

--- Input.py
+++ Input.py
@@ -1,42 +1,42 @@
 from dataclasses import dataclass
 from typing import List
 
-from Street import Street
 from Route import Route
+from Street import Street
 
 
 @dataclass
 class Input:
+    name: str
     simTime: int
-    intersections: List
+    streets: dict
     routes: List
+    scorePerCar: int
 
     @staticmethod
     def parse(filename):
-        lineCount =
-        with open("input/" + filename, "r") as file:
+        lineCount = len(open("input/" + filename + ".txt", "r").readlines())
+        with open("input/" + filename + ".txt", "r") as file:
             inputInfoLine = file.readline().strip("\n").split(" ")
             assert (len(inputInfoLine) == 5)
 
-            simTime, numIntersections, numStreets, numCars, score = inputInfoLine
+            simTime, numIntersections, numStreets, numCars, scorePerCar = inputInfoLine
 
-            intersections = []
-            for _ in range(0, int(numIntersections)): # read in the intersections
+            intersections = {}
+            for _ in range(0, lineCount - int(numCars) - 1):  # read in the intersections
                 intersectionInfoLine = file.readline().strip("\n").split(" ")
                 assert (len(intersectionInfoLine) == 4)
 
-                intersections.append(Street(int(intersectionInfoLine[0]),
+                intersections[intersectionInfoLine[2]] = Street(int(intersectionInfoLine[0]),
                                             int(intersectionInfoLine[1]),
                                             intersectionInfoLine[2],
-                                            int(intersectionInfoLine[3])))
+                                            int(intersectionInfoLine[3]))
 
             routes = []
-            for route in reversed(open("input/" + filename, "r").readlines()): # Open the file a second time
-                routeInfoLine = route
+            for _ in range(0, int(numCars) - 1):
+                routeInfoLine = file.readline().strip("\n").split(" ")
                 numSteps = int(routeInfoLine[0])
-                assert(numSteps == len(routeInfoLine) + 1)
+                assert (numSteps == len(routeInfoLine) - 1)
                 routes.append(Route(routeInfoLine[1:]))
 
-
-            return Input(simTime, intersections, routes)
-
+            return Input(filename, int(simTime), intersections, routes, int(scorePerCar))
